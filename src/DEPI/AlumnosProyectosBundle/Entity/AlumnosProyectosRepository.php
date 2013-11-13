eb28944d81aea128dd8d3bac58d7b30c694ddec4
Jorge Atempa <atempaitm@gmail.com>
2013-11-12 17:19:56 -0800

    Repositorio de Alumnos vinculados a Proyectos

---
+++ src/DEPI/AlumnosProyectosBundle/Entity/AlumnosProyectosRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DEPI\AlumnosProyectosBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class AlumnosProyectosRepository extends EntityRepository
+{
+	public function findAlumnosConProyecto()
+	{
+		
+	}
+}
