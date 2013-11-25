78f3d93a33c65514e939b843e4adf26ffa4318ad
Jorge Atempa <jatempa@itmexicali.edu.mx>
2013-11-24 21:02:17 -0800

    Restaurando controlador del backend

--- src/DEPI/BackEndBundle/Controller/DefaultController.php
+++ src/DEPI/BackEndBundle/Controller/DefaultController.php
@@ -6,5 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	public function indexAction()
+    {
+    	return $this->render('BackEndBundle:Default:index.html.twig');
+    }
   
 }
