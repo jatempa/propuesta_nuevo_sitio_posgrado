f2f1283f23f8fda89935580e7912e5f0a97b9776
Jorge Atempa <jatempa@itmexicali.edu.mx>
2013-12-07 14:54:35 -0800

    Dimensiones de la consulta de imagenes para el banner

--- src/DEPI/PortadaBundle/Controller/PortadaController.php
+++ src/DEPI/PortadaBundle/Controller/PortadaController.php
@@ -77,7 +77,7 @@ class PortadaController extends Controller
             'method' => 'POST',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+        $form->add('submit', 'submit', array('label' => 'Guardar'));
 
         return $form;
     }
@@ -141,7 +141,7 @@ class PortadaController extends Controller
             'method' => 'PUT',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+        $form->add('submit', 'submit', array('label' => 'Actualizar'));
 
         return $form;
     }
@@ -217,7 +217,7 @@ class PortadaController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('portada_delete', array('id' => $id)))
             ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->add('submit', 'submit', array('label' => 'Eliminar'))
             ->getForm()
         ;
     }
