d97a0a130f9fefacb21c994918b0873b34a06ddf
Jorge Atempa <jatempa@itmexicali.edu.mx>
2013-12-12 16:30:19 -0800

    Vista de areas

---
+++ src/DEPI/AreasBundle/Controller/AreasPublicController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DEPI\AreasBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use DEPI\AreasBundle\Entity\Areas;
+
+/**
+ * AreasPublic controller.
+ *
+ * @Route("/areas_public")
+ */
+class AreasPublicController extends Controller
+{
+
+    /**
+     * Lists all Areas entities.
+     *
+     * @Route("/", name="areas_public")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AreasBundle:Areas')->findAll();
+        $banner = $em->getRepository('PortadaBundle:Portada')->findImagenesBanner();
+
+        return $this->render('AreasBundle:Areas:index_public.html.twig', array('entities' => $entities, 'banner' => $banner));
+    }
+}
