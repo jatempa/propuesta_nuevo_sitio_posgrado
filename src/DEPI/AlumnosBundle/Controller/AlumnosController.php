71d2e516e67118b740efcc43b78542ff856409f6
Jorge Atempa <jatempa@itmexicali.edu.mx>
2013-12-27 21:52:50 -0800

    Se refactorizo metodo de actualizacion

--- src/DEPI/AlumnosBundle/Controller/AlumnosController.php
+++ src/DEPI/AlumnosBundle/Controller/AlumnosController.php
@@ -34,14 +34,14 @@ class AlumnosController extends Controller
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($alumnos, $this->get('request')->query->get('page',1), 5);
 
-        return $this->render('AlumnosBundle:Alumnos:index.html.twig', array('alumnos' => $pagination));
+        return array('alumnos' => $pagination);
     }
     /**
      * Creates a new Alumnos entity.
      *
      * @Route("/", name="alumnos_new")
      * @Method("POST")
-     * @Template("AlumnosBundle:Alumnos:new.html.twig")
+     * @Template()
      */
     public function newAction()
     {
@@ -59,65 +59,17 @@ class AlumnosController extends Controller
             return $this->redirect($this->generateUrl('alumnos'));
         }
 
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
+        return array('entity' => $entity, 'form' => $form->createView());
     }
 
-    /**
-     * Displays a form to edit an existing Alumnos entity.
-     *
-     * @Route("/{id}/edit", name="alumnos_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('AlumnosBundle:Alumnos')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Alumnos entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Alumnos entity.
-    *
-    * @param Alumnos $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Alumnos $entity)
-    {
-        $form = $this->createForm(new AlumnosType(), $entity, array(
-            'action' => $this->generateUrl('alumnos_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Actualizar'));
-
-        return $form;
-    }
     /**
      * Edits an existing Alumnos entity.
      *
-     * @Route("/{id}", name="alumnos_update")
+     * @Route("/{id}", name="alumnos_edit")
      * @Method("PUT")
-     * @Template("AlumnosBundle:Alumnos:edit.html.twig")
+     * @Template()
      */
-    public function updateAction(Request $request, $id)
+    public function editAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -127,11 +79,14 @@ class AlumnosController extends Controller
             throw $this->createNotFoundException('Unable to find Alumnos entity.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
+        $form = $this->createForm(new AlumnosType(), $entity, array(
+            'action' => $this->generateUrl('alumnos_edit', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
 
-        if ($editForm->isValid()) {
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
             $entity->subirFoto($this->container->getParameter('alumnos.directorio.imagenes'));
             $em->flush();
 
@@ -140,50 +95,7 @@ class AlumnosController extends Controller
 
         return array(
             'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'form'   => $form->createView(),
         );
-    }
-    /**
-     * Deletes a Alumnos entity.
-     *
-     * @Route("/{id}", name="alumnos_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('AlumnosBundle:Alumnos')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Alumnos entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('alumnos'));
-    }
-
-    /**
-     * Creates a form to delete a Alumnos entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('alumnos_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Eliminar'))
-            ->getForm()
-        ;
     }
 }
