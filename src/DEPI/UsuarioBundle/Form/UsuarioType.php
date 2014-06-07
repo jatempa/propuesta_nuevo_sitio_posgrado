6410a6616c107469bc68b9c6fdc93feb2ad2229a
Jorge Atempa <atempaitm@gmail.com>
2014-06-07 00:48:05 -0700

    Se genero crud para usuarios y se removio contenido default

---
+++ src/DEPI/UsuarioBundle/Form/UsuarioType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DEPI\UsuarioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UsuarioType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('password')
+            ->add('email')
+            ->add('permiteEmail')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DEPI\UsuarioBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'depi_usuariobundle_usuario';
+    }
+}
