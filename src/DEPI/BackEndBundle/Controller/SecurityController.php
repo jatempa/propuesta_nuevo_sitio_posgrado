7de529fba6d6069140569346c7430677d90f825d
Jorge Atempa <atempaitm@gmail.com>
2014-06-21 16:17:20 -0700

    Se modifico pagina de login

--- src/DEPI/BackEndBundle/Controller/SecurityController.php
+++ src/DEPI/BackEndBundle/Controller/SecurityController.php
@@ -2,13 +2,47 @@
 
 namespace DEPI\BackEndBundle\Controller;
 
-use \FOS\UserBundle\Controller\SecurityController as BaseSecurityController;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
-class SecurityController extends BaseSecurityController
+class SecurityController extends Controller
 {
     public function indexAction()
     {
         return $this->render('BackEndBundle:Security:index.html.twig');
+    }
+
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        return $this->render('BackEndBundle:Security:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+        ));
     }
 }
