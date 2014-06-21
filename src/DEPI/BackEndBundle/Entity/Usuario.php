0f3f4faf8e7fb1b34050fedcb9d01fad6fdb20d8
Jorge Atempa <atempaitm@gmail.com>
2014-06-21 13:03:51 -0700

    Se agrego clase para el mapeo de datos de usuarios

---
+++ src/DEPI/BackEndBundle/Entity/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DEPI\BackEndBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table(name="usuarios")
+ * @ORM\Entity
+ */
+class Usuario extends BaseUser
+{
+	/**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+	protected $id;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
