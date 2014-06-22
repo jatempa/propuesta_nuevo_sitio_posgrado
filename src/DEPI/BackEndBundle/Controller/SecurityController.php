cc759e35da692eeaa95ececf565ed7283cff7fa2
Jorge Atempa <atempaitm@gmail.com>
2014-06-21 17:20:18 -0700

    Se agregaron el resto de funciones para sobreescribir el controlador de seguridad

--- src/DEPI/BackEndBundle/Controller/SecurityController.php
+++ src/DEPI/BackEndBundle/Controller/SecurityController.php
@@ -45,4 +45,29 @@ class SecurityController extends Controller
             'csrf_token' => $csrfToken,
         ));
     }
+
+    /**
+     * Renders the login template with the given parameters. Overwrite this function in
+     * an extended controller to provide additional data for the login template.
+     *
+     * @param array $data
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function renderLogin(array $data)
+    {
+        $template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
+
+        return $this->container->get('templating')->renderResponse($template, $data);
+    }
+
+    public function checkAction()
+    {
+        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
+    }
+
+    public function logoutAction()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
 }
